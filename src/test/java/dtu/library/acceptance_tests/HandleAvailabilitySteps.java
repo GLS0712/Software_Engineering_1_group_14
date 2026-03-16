ee93f60c05803530f4e50859b98381cb8954b3a0
GedeGustav <gustavsvare@gmail.com>
2026-03-16 16:20:40 +0100

    fixed tests setup

--- src/test/java/dtu/library/acceptance_tests/HandleAvailabilitySteps.java
+++ src/test/java/dtu/library/acceptance_tests/HandleAvailabilitySteps.java
@@ -12,31 +12,31 @@ import io.cucumber.java.en.When;
 
 public class HandleAvailabilitySteps {
 
-@Given("An employe is added to an activity")
+@Given("An {string} is added to an activity")
 public void anEmployeIsAddedToAnActivity() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
-@Given("the employe has {int} active activities")
+@Given("the {string} has {int} active activities")
 public void theEmployeHasActiveActivities(Integer int1) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
-@When("the employe gets assigned")
+@When("the {string} gets assigned")
 public void theEmployeGetsAssigned() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
-@Then("the employe is added to the activity")
+@Then("the {string} is added to the activity")
 public void theEmployeIsAddedToTheActivity() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
-@Then("the employe is not added to the activity")
+@Then("the {string} is not added to the activity")
 public void theEmployeIsNotAddedToTheActivity() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
--- use_cases/provided/HandleAvailability.feature
+++ use_cases/provided/HandleAvailability.feature
@@ -1,14 +1,14 @@
-Feature: is an employe available
-    Description: handles employes availability when assigned multible activities
+Feature: is an employee available
+    Description: handles an employees availability when assigned multible activities
 
 Scenario: is added to an activity when available
-Given An employe is added to an activity
-And the employe has 0 active activities
-When the employe gets assigned
-Then the employe is added to the activity
+Given An "employee" is added to an activity
+And the "employee" has 0 active activities
+When the "employee" gets assigned
+Then the "employee" is added to the activity
 
 Scenario: is added to an activity when not available
-Given An employe is added to an activity
-And the employe has 10 active activities
-When the employe gets assigned
-Then the employe is not added to the activity
+Given An "employee" is added to an activity
+And the "employee" has 10 active activities
+When the "employee" gets assigned
+Then the "employee" is not added to the activity
