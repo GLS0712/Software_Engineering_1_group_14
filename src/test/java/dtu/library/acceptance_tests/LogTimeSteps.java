fcd2a981ffc3bb5e4266fbe766f7c43b41472696
GedeGustav <gustavsvare@gmail.com>
2026-03-16 16:32:17 +0100

    Scenario added

--- src/test/java/dtu/library/acceptance_tests/LogTimeSteps.java
+++ src/test/java/dtu/library/acceptance_tests/LogTimeSteps.java
@@ -10,27 +10,38 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LogTimeSteps {
-    
-@Given("An {string} has finished an activity")
-public void anHasFinishedAnActivity(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
+public class LogTimeSteps { 
+    @Given("An {string} has finished an activity")
+    public void anHasFinishedAnActivity(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
+    @When("the {string} logs that they have spend {int} hours")
+    public void theLogsThatTheyHaveSpendHours(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
-@When("the {string} logs that they have spend {int} hours")
-public void theLogsThatTheyHaveSpendHours(String string, Integer int1) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
+    @Then("it should be logged that {int} hours have been spend")
+    public void itShouldBeLoggedThatHoursHaveBeenSpend(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
+    @Then("{string} should be notified of incorrect time logged")
+    public void should_be_notified_of_incorrect_time_logged(String s) {
+        // Write code here that turns the phrase above into concrete actions
+    }
 
-@Then("it should be logged that {int} hours have been spend")
-public void itShouldBeLoggedThatHoursHaveBeenSpend(Integer int1) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
+    @Then("no time should be logged")
+    public void no_time_should_be_logged() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Given("An {string} has finished an activity with total time of {int} days")
+    public void An_has_finished_an_activity_with_total_time_of_day(String s, int i) {
+        // Write code here that turns the phrase above into concrete actions
+    }
 	
-    
 }
